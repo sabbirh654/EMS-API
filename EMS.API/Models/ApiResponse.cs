0da1f81eb544aaed8e6e05ac160783612b4027f8
Sabbir Hasan <sabbir.hasan@technonext.com>
2024-11-27 00:44:22 +0600

    Initial project setup

---
+++ EMS.API/Models/ApiResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace EMS.API.Models;
+
+public class ApiResponse<T>
+{
+    public bool Success { get; set; }
+    public string? Message { get; set; }
+    public T? Result { get; set; }
+}
---
+++ EMS.API/ServiceExtensions.cs
@@ -0,0 +1,20 @@
+﻿using EMS.Repository.DatabaseProviders;
+using EMS.Repository.Implementations;
+using EMS.Repository.Interfaces;
+
+namespace EMS.API;
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection RegisterServices(this IServiceCollection services)
+    {
+        services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+        services.AddScoped<IDesignationRepository, DesignationRepository>();
+        services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+        services.AddScoped<IAttendanceRepository, AttendanceRepository>();
+
+        services.AddSingleton<IDatabaseFactory, DatabaseFactory>();
+
+        return services;
+    }
+}
